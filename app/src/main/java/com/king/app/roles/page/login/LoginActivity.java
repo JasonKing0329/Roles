92b6b1f0fd44f84f6a3437e37785100ce245336c
JingYang <jingyang@tcsl.com.cn>
2019-08-02 16:07:17 +0800

    v3.1 support android common fingerprint and samsung fingerprint

--- app/src/main/java/com/king/app/roles/page/login/LoginActivity.java
+++ app/src/main/java/com/king/app/roles/page/login/LoginActivity.java
@@ -1,24 +1,22 @@
 package com.king.app.roles.page.login;
 
 import android.Manifest;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.support.annotation.Nullable;
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 
 import com.king.app.roles.R;
 import com.king.app.roles.base.MvvmActivity;
 import com.king.app.roles.databinding.ActivityLoginBinding;
-import com.king.app.roles.model.FingerPrintController;
+import com.king.app.roles.model.fingerprint.samsung.SamsungFingerPrint;
 import com.king.app.roles.page.story.StoryListActivity;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
 
 public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewModel> {
 
-    private FingerPrintController fingerPrint;
+    private SamsungFingerPrint fingerPrint;
 
     @Override
     protected int getContentView() {
@@ -28,28 +26,10 @@ public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewM
     @Override
     protected void initView() {
         binding.setViewModel(viewModel);
-        viewModel.fingerprintObserver.observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(@Nullable Boolean aBoolean) {
-                fingerPrint = new FingerPrintController(LoginActivity.this);
-                if (fingerPrint.isSupported()) {
-                    if (fingerPrint.hasRegistered()) {
-                        startFingerPrintDialog();
-                    } else {
-                        showMessageLong("设备未注册指纹");
-                    }
-                    return;
-                } else {
-                    showMessageLong("设备不支持指纹识别");
-                }
-            }
-        });
-        viewModel.loginObserver.observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(@Nullable Boolean success) {
-                if (success) {
-                    startHome();
-                }
+        viewModel.fingerprintObserver.observe(this, aBoolean -> checkFingerprint());
+        viewModel.loginObserver.observe(this, success -> {
+            if (success) {
+                startHome();
             }
         });
     }
@@ -65,19 +45,11 @@ public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewM
                 .request(Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Boolean>() {
-                    @Override
-                    public void accept(Boolean isGranted) throws Exception {
-                        if (isGranted) {
-                            initCreate();
-                        }
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        throwable.printStackTrace();
-                        finish();
-                    }
+                .subscribe(isGranted -> {
+                    initCreate();
+                }, throwable -> {
+                    throwable.printStackTrace();
+                    finish();
                 });
     }
 
@@ -85,29 +57,67 @@ public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewM
         viewModel.initCreate();
     }
 
-    private void startFingerPrintDialog() {
-        if (fingerPrint.hasRegistered()) {
-            boolean withPW = false;
-            fingerPrint.showIdentifyDialog(withPW, new FingerPrintController.SimpleIdentifyListener() {
-
-                @Override
-                public void onSuccess() {
-                    startHome();
-                }
-
-                @Override
-                public void onFail() {
-
-                }
-
-                @Override
-                public void onCancel() {
-                    finish();
-                }
-            });
-        } else {
-            showMessageLong(getString(R.string.login_finger_not_register));
+    private void checkFingerprint() {
+        FingerprintManagerCompat compat = FingerprintManagerCompat.from(this);
+        if (compat.isHardwareDetected()) {
+            if (compat.hasEnrolledFingerprints()) {
+                startFingerPrintDialog();
+            }
+            else {
+                showMessageLong("设备未注册指纹");
+            }
         }
+        else {
+            // 三星Tab S(Android6.0)有指纹识别，但是系统方法判断为没有，继续用三星的sdk操作
+            checkSamsungFingerprint();
+        }
+    }
+
+    /**
+     * 通用指纹识别对话框
+     */
+    private void startFingerPrintDialog() {
+        FingerprintDialog dialog = new FingerprintDialog();
+        dialog.setOnFingerPrintListener(() -> startHome());
+        dialog.show(getSupportFragmentManager(), "FingerprintDialog");
+    }
+
+    private void checkSamsungFingerprint() {
+        fingerPrint = new SamsungFingerPrint(LoginActivity.this);
+        if (fingerPrint.isSupported()) {
+            if (fingerPrint.hasRegistered()) {
+                startSamsungFingerPrintDialog();
+            } else {
+                showMessageLong("设备未注册指纹");
+            }
+            return;
+        } else {
+            showMessageLong("设备不支持指纹识别");
+        }
+    }
+
+    /**
+     * 三星指纹识别对话框
+     */
+    private void startSamsungFingerPrintDialog() {
+        boolean withPW = false;
+        fingerPrint.showIdentifyDialog(withPW, new SamsungFingerPrint.SimpleIdentifyListener() {
+
+            @Override
+            public void onSuccess() {
+                startHome();
+            }
+
+            @Override
+            public void onFail() {
+
+            }
+
+            @Override
+            public void onCancel() {
+                finish();
+            }
+        });
     }
 
     private void startHome() {
